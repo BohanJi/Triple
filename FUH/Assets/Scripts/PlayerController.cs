9135f90d0d8538705b1ac1e8c7412e837c24b100
BohanJi <al405281@uji.es>
2022-11-17 17:39:04 +0100

    init

--- FUH/Assets/Scripts/PlayerController.cs
+++ FUH/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,12 +8,18 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigidbody2d;
     List<string> ActivesItems = new();
+    List<DialogueLine> dialogueList = new();
 
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(this);
         rigidbody2d = GetComponent<Rigidbody2D>();
+        
+        ReadDataCSV rd = new();
+        rd.ReadFileData();
+        rd.ExtractData();
+        dialogueList = rd.dialogueList;
     }
 
     // Update is called once per frame
@@ -41,4 +48,68 @@ public class PlayerController : MonoBehaviour
     {
         return ActivesItems;
     }
+    public List<DialogueLine> GetDialogueLines() 
+    {
+        return dialogueList;
+    }
+}
+
+public class ReadDataCSV
+{
+    public string datosString;
+
+    public string dataPathAssets;
+    public string DialogueDataName = "/Dialogos.csv";
+    //private string persistentDataPath;
+
+    public string header;
+    public string[] lines;
+    public List<DialogueLine> dialogueList = new();
+
+    public void ReadFileData()
+    {
+        dataPathAssets = Application.dataPath;
+        //persistentDataPath = Application.persistentDataPath;
+        datosString = "";
+
+        StreamReader sr = new(path: dataPathAssets + DialogueDataName);
+        string line;
+
+        while ((line = sr.ReadLine()) != null)
+        {
+            datosString += line + "\n";
+        }
+    }
+
+    public void ExtractData()
+    {
+        lines = datosString.Split('\n');
+        header = lines[0];
+
+        dialogueList.Clear();
+
+        for (int i = 1; i < lines.Length - 1; i++)
+        {
+            string[] splittedLine = lines[i].Split(';');
+
+            if (splittedLine.Length == 4)
+            {
+                dialogueList.Add(new DialogueLine()
+                {
+                    title = splittedLine[0],
+                    lineNumber = int.Parse(splittedLine[1]),
+                    role = splittedLine[2],
+                    dialogue = splittedLine[3]
+                });
+            }
+        }
+    }
+}
+
+public struct DialogueLine
+{
+    public string title;
+    public int lineNumber;
+    public string role;
+    public string dialogue;
 }
